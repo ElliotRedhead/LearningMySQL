c9aaa09e9a560ee4f6b60914154a1b1bb12731d1
unknown <elliot.redhead@gmail.com>
2019-11-12 21:50:52 +0000

    Implemented course video content.

--- test.sql
+++ test.sql
@@ -1,3 +1,12 @@
 select count(*) from Track;
 select count(*) from Artist;
 select Name from Artist;
+select * from Track
+WHERE Composer = "U2";
+select Name, Title, ArtistId from Track
+INNER JOIN Album on Track.AlbumId = Album.AlbumId;
+SELECT Track.Name as Track, Title as Album, ArtistId, Artist.Name as Artist from Track
+INNER JOIN Album on Track.AlbumId = Album.AlbumId
+INNER JOIN Artist on Album.ArtistId = Artist.ArtistId
+WHERE Artist.Name = "U2"
+ORDER BY Album.Title desc, Track.Name;
