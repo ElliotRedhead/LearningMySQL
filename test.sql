2b338a9eb1850010a52550867166b61736066f50
ElliotRedhead <elliot.redhead@gmail.com>
2019-11-15 22:28:57 +0000

    Made additions with CI course.

--- test.sql
+++ test.sql
@@ -11,3 +11,20 @@ INNER JOIN Artist on Album.ArtistId = Artist.ArtistId
 WHERE Artist.Name = "U2"
 ORDER BY Album.Title desc, Track.Name;
 SELECT MIN(LastName) FROM Customer;
+SHOW COLUMNS FROM Employee;
+
+-- ROUND(column or expression,precision)
+-- SUM(column or expression)
+
+SELECT Album.Title, COUNT(*) FROM Track
+INNER JOIN Album ON Track.AlbumId = Album.AlbumId
+GROUP BY Track.AlbumId;
+
+SELECT Album.Title, ROUND(SUM(UnitPrice),2) FROM Track
+INNER JOIN Album ON Track.AlbumId = Album.AlbumId
+GROUP BY Track.AlbumId;
+
+INSERT INTO MediaType (Name)
+VALUES ("Test Media Type 1");
+
+DELETE FROM MediaType WHERE Name = "Test Media Type 1";
