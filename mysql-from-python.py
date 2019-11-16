a5f63bb9f65695b4e5286cc00b8d561a0ec4c60c
ElliotRedhead <elliot.redhead@gmail.com>
2019-11-16 16:40:06 +0000

    Create the "Friends" table in the database.

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import pymysql
 
 username = "Elliot"
@@ -6,7 +7,10 @@ username = "Elliot"
 connection =  pymysql.connect(host='localhost', user=username, password='T3STsqlpass$!', db="Chinook")
 
 try:
-    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+    with connection.cursor() as cursor:
+        cursor.execute("""CREATE TABLE IF NOT EXISTS
+                        Friends(name char(20), age int, DOB datetime);""")
+        # The above  will display a warning if the table already exists.
         sql = "SELECT * FROM Artist;"
         cursor.execute(sql)
         for row in cursor:
